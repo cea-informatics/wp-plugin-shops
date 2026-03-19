393b23d8b2a7d69b6a21daab0812be574abbfc1d
ClaireProd <claire@inetis.ch>
2026-03-19 10:36:02 +0100

    [version] Update to 1.0.5

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Custom Shops
  * Description:     The plugin is responsible of stores management.
- * Version:         1.0.4
+ * Version:         1.0.5
  * Author:          CEA Informatics
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,7 +14,7 @@
 
 if (!defined('ABSPATH')) exit;
 
-define('WPS_VERSION', '1.0.4');
+define('WPS_VERSION', '1.0.5');
 define('WPS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WPS_PLUGIN_URL', plugin_dir_url(__FILE__));
 
@@ -23,9 +23,12 @@ require_once WPS_PLUGIN_DIR . 'includes/class-wps-admin.php';
 
 register_activation_hook(__FILE__, array('WPS_DB', 'install'));
 
-WPS_Admin::init();
+// Initialize admin class if available
+if (class_exists('WPS_Admin')) {
+    WPS_Admin::init();
+}
 
-function wpw_display_shops() {
+function wps_display_shops() {
     ob_start(); ?>
     <div id="wp-shops">
     <button id="wp-shops">shops Info</button>
@@ -33,5 +36,12 @@ function wpw_display_shops() {
     return ob_get_clean();
 }
 
-add_shortcode('wp-shops', 'wpw_display_shops');
+function wps_enqueue_scripts() {
+    wp_enqueue_style('wps-style', WPS_PLUGIN_URL . 'assets/shops.css', array(), WPS_VERSION);
+    wp_enqueue_script('wps-script', WPS_PLUGIN_URL . 'assets/shops.js', WPS_VERSION, true);
+}
+
+add_action('wp_enqueue_scripts', 'wps_enqueue_scripts');
+
+add_shortcode('wp-shops', 'wps_display_shops');
 
